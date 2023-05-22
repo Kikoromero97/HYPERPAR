180c27e29864eed8d03edeb930d375d3de20ce02
Kikoromero97 <kikoromero1997@gmail.com>
2023-05-22 19:18:53 +0200

    Septimo commit - Comentarios de Documento subidos.

--- Algoritmes/src/es/iesmz/ed/algoritmes/Any.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/Any.java
@@ -3,13 +3,31 @@ package es.iesmz.ed.algoritmes;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Esta clase comprueba cuántos digitos diferentes hay en un número.
+ *
+ * @author: Francisco Romero Gálvez
+ * @version: 22/05/2023
+ */
+
+
 public class Any {
     private final long numero;
 
+    /**
+     * Constructor para pasar el año pedido.
+     *
+     * @param numero El parámetro define el año que se va a comprobar.
+     */
     public Any(long numero) {
         this.numero = numero;
     }
 
+    /**
+     * Método que comprueba la cantidad de dígitos del número pasado que son diferentes.
+     *
+     * @return Devuelve la cantidad de dígitos que son diferentes.
+     */
     public int digitsDiferents() {
         List<Character> digitsList = new ArrayList<>();
         String anyoStr = String.valueOf(numero);
