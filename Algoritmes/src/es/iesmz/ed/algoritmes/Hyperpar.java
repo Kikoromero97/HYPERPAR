7f7f01a3c94a1be0e2ff89a021e1fc7163da85ca
Kikoromero97 <kikoromero1997@gmail.com>
2023-05-22 18:24:41 +0200

    Tercer commit - Añadido documentación.

--- Algoritmes/src/es/iesmz/ed/algoritmes/Hyperpar.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/Hyperpar.java
@@ -1,12 +1,30 @@
 package es.iesmz.ed.algoritmes;
 
+/**
+ * Esta clase compara si un número introducido y todos sus digitos (digito a digito) son pares.
+ *
+ * @author: Francisco Romero Gálvez
+ * @version: 22/05/2023
+ */
+
 public class Hyperpar {
     private long numero = 0;
 
+    /**
+     * Constructor para pasar el número pedido.
+     *
+     * @param numero El parámetro define el número que se quiere comprobar si es par o no.
+     */
     public Hyperpar(long numero) {
         this.numero = numero;
     }
-    public boolean esHyperPar () {
+
+    /**
+     * Método que devuelve si los dígitos del número pasado son pares o impares.
+     *
+     * @return Devuelve false si alguno de los dígitos del número pasado son impares. Devuelve true si todos los dígitos son pares.
+     */
+    public boolean esHyperPar() {
         int contadorNoHyper = 0;
         String numeroStr = String.valueOf(numero);
         boolean esHyperPar = true;
